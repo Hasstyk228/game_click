a4d1bd41fc159eac40ad5173950df8ee413dde45
Hasstyk228 <radu0833@gmail.com>
2025-04-14 23:21:06 +0300

    2 version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef , useState } from "react";
+import { useRef , useState , useEffect } from "react";
 import React from 'react';
 import './App.css';
 import './data.js'
@@ -30,9 +30,29 @@ export default function App() {
 
   const [upAuto, setUpAuto] = useState(100);
 
+  const [coords, setCoords] = useState(null);
+
+  const [show, setShow] = useState(true);
+
+
 /*///////////////////////////////////////////////////////*/
 
-  function addPoint () {setPoint(param => param+dimamicPoint) }
+  function addPoint (e) {
+    setPoint(param => param+dimamicPoint) 
+    
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      setCoords({ x, y });
+      
+  }
+
+  useEffect(() => {
+    if (coords) {
+      const timer = setTimeout(() => {setCoords(null)}, 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [coords]);
 
   function autoClick () { setPoint(param => param + valueAutoCLick.current) }
 
@@ -41,6 +61,8 @@ export default function App() {
   function upValTime() { setValueTime(param => param*2) }
 
 /*///////////////////////////////////////////////////////*/
+
+
 
   function upgradeAutoClick () {
 
@@ -110,12 +132,48 @@ export default function App() {
 function isActiveAutoClick () {
   if(active=="off")
   {
-    return "Value A/C : 1000 P"
+    return "1000 P"
   } else {
-    return "AutoClick On"
+    return "On"
   }
 }
   
+/*------------------------------------*/  
+
+function endText (value) {
+
+  if(value > 0 && value < 1000 )
+  {
+    return format(Math.round(value))
+
+  } else if(value /1000 > 1 && value /1000 < 1000)
+  {
+    return (Math.round(value)/1000).toFixed(1) + " K"
+
+  } else if(value /1000000 > 1 && value /1000000 < 1000)
+  {
+    return (Math.round(value)/1000000).toFixed(1) + " M"
+  
+  } else if(value /1000000000 > 1 && value /1000000000 < 1000)
+  {
+    return (Math.round(value)/1000000000).toFixed(1) + " B"
+
+  } else if(value /1000000000000 > 1 && value /1000000000000 < 1000)
+  {
+      return (Math.round(value)/1000000000000).toFixed(1) + " T"
+
+  } else if(value /1000000000000000 > 1 && value /1000000000000000 < 1000)
+  {
+      return (Math.round(value)/1000000000000000).toFixed(1) + " Q"
+
+  } else if(value /1000000000000000000 > 1 && value /1000000000000000000 < 1000)
+  {
+    return (Math.round(value)/1000000000000000000).toFixed(1) + " Q"
+  } 
+  
+
+}
+
 /*///////////////////////////////////////////////////////*/
 
   function format(num, fix) {
@@ -148,7 +206,11 @@ function isActiveAutoClick () {
           <div id="b2_info">
             <div id="b2_i1">
               <div id="b2_i1_1">Click</div>
-              <div id="b2_i1_2">{format(Math.round(dimamicPoint))} P</div>
+              <div id="b2_i1_2" >
+                
+                {endText(dimamicPoint)} [P]
+                
+                </div>
               <button id="b2_i_btn" onClick={upgradeClick}>{"<"}Up{">"}</button>
             </div>
             <div id="b2_i2">
@@ -158,17 +220,65 @@ function isActiveAutoClick () {
             </div>
             <div id="b2_i3">
               <div id="b2_i3_1" onClick={startAutoClick}>Auto-C: {active}</div>
-              <div id="b2_i3_3">{format(Math.round(upAuto))} P/s</div>
+              <div id="b2_i3_3">
+                {endText(upAuto)} [P/s]
+              </div>
               <div id="b2_div_btn3"><button id="b2_i3_btn" onClick={upgradeAutoClick} >{"<"}Up{">"}</button>
               </div>
             </div>
           </div>
           
           <div id="b2_click" onClick={addPoint}> 
-            <div>            Value Click : {format(Math.round(autoPoint))} P</div>
-            <div>Value Time : {format(Math.round(valueTime))} P</div>
-            <div>{isActiveAutoClick()}</div>
-            <div>Value P/S : {format(Math.round(upValAuto))} P</div>
+            <div id="vl_click_div">   
+              <p>{format(Math.round(autoPoint))} P</p>
+              <label htmlFor="">Value Click</label>
+            </div>
+
+            <div id="vl_click_div2">
+              <p>{format(Math.round(valueTime))} P</p>
+              <label htmlFor="">Value Time</label>
+            </div>
+
+            <div id="vl_click_div2">
+              <p>{format(Math.round(upValAuto))} P</p>
+              <label htmlFor="">Value p/s</label>
+            </div>
+            
+            <div id="vl_click_div2">
+              <p>{isActiveAutoClick()}</p>
+              <label htmlFor="">Auto Click</label>
+            </div>
+
+            <div >
+              {coords && (
+                <div
+                  className="absolute text-red-600 font-bold"
+                  style={{
+                    width:'60px',
+                    height:'60px',
+                    left: coords.x,
+                    top: coords.y,
+                    fontWeight:'800',
+                    transform: 'translate(-90%, -480%)',
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'center',
+                    paddingLeft:"0px",
+                    color:'rgb(195, 133, 0)',
+                    border:'3px solid rgb(199, 136, 0)',
+                    borderRadius:'100px',
+                    background: 'linear-gradient(140deg,rgba(255, 221, 0, 1) 22%, rgb(255, 243, 176) 35%, rgb(255, 251, 182) 38%, rgba(255, 225, 0, 1) 51%, rgba(255, 225, 0, 1) 35%, rgb(254, 247, 170) 58%, rgb(254, 255, 191) 61%, rgba(255, 225, 0, 1) 73%)',
+                    boxShadow: '1px 0px 0px 1px rgb(158, 116, 0)',
+                    fontSize: 'clamp(12px, 3vw, 32px)',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                   // backgroundColor:"rgb(255, 225, 0)",
+                    }}> 
+
+                  {"+" + endText(dimamicPoint)}</div> )
+            }
+          </div>
+
           </div>
         </div>
       </div>
